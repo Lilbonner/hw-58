c18281028325dee5e6c531ec4e48427fc42f4b4d
Daniyar Nasirov <lilbonnie666@gmail.com>
2023-11-22 17:54:14 +0600

    add children into props in modal & alerts

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -1,19 +1,20 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 interface AlertProps {
-  type: 'primary' | 'success' | 'danger' | 'warning';
-  onDismiss?: () => void;
+    type: 'primary' | 'success' | 'danger' | 'warning';
+    onDismiss?: () => void;
+    children?: ReactNode;
 }
 
 const Alert: FC<AlertProps> = ({ type, onDismiss, children }) => (
-  <div className={`alert alert-${type} alert-dismissible d-flex`} role="alert">
-    {children}
-    {onDismiss && (
-      <button type="button" className="close bg-transparent border-0 ms-auto" data-dismiss="alert" aria-label="Close" onClick={onDismiss}>
-        <span aria-hidden="true">&times;</span>
-      </button>
-    )}
-  </div>
+    <div className={`alert alert-${type} alert-dismissible d-flex`} role="alert">
+        {children}
+        {onDismiss && (
+            <button type="button" className="close bg-transparent border-0 ms-auto" data-dismiss="alert" aria-label="Close" onClick={onDismiss}>
+                <span aria-hidden="true">&times;</span>
+            </button>
+        )}
+    </div>
 );
 
 export default Alert;
--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,25 +1,34 @@
-import React, { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 interface ModalProps {
-  show: boolean;
-  onClose: () => void;
-  title: string;
+    show: boolean;
+    onClose: () => void;
+    title: string;
+    children: ReactNode;
 }
 
-const Modal: FC<ModalProps> = ({ show, onClose, title, children }) => (
-  <div className={`modal fade ${show ? 'show' : ''}`} style={{ display: show ? 'block' : 'none' }}>
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">{title}</h5>
-          <button type="button" className="close bg-transparent border-0" onClick={onClose}>
-            <span>&times;</span>
-          </button>
-        </div>
-        <div className="modal-body">{children}</div>
-      </div>
-    </div>
-  </div>
-);
+const Modal: FC<ModalProps> = ({ show, onClose, title, children }) => {
+    const modalStyle: React.CSSProperties = {
+        display: show ? 'block' : 'none',
+    };
+
+    return (
+        <>
+            <div className={`modal fade ${show ? 'show' : ''}`} style={modalStyle}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{title}</h5>
+                            <button type="button" className="close bg-transparent border-0" onClick={onClose}>
+                                <span>&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">{children}</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default Modal;
